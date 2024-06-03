b67441bc7eef879ea4e9e25b9b402f4161f4bc19
kevin cuadros <kevindavidcuadros@gmail.com>
2024-06-03 11:33:17 -0500

    chore: Update StackNavigation to use typed navigation params

--- src/presentation/navigation/StackNavigation.tsx
+++ src/presentation/navigation/StackNavigation.tsx
@@ -2,11 +2,18 @@ import { createStackNavigator } from '@react-navigation/stack'
 import HomeScreen from '../screens/home/HomeScreen'
 import DetailsScreen from '../screens/details/DetailsScreen'
 
-const Stack = createStackNavigator()
+export type RootParams = {
+  Home: undefined,
+  Details: { movieId: number }
+}
+
+const Stack = createStackNavigator<RootParams>()
 
 const StackNavigation = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{
+      headerShown: false,
+    }}>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Details" component={DetailsScreen} />
     </Stack.Navigator>
